192ee1fa398432ff91fcf3b490370da32f84beec
Jenesius <lokargenia@gmail.com>
2020-01-06 23:07:41 +0300

    Добавление Vuex для ВК.

--- mobile/src/store/index.js
+++ mobile/src/store/index.js
@@ -1,22 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import vk from './modules/vk.js';
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    vk:{
-      idUser:Number,
-      music:{
-        online:{},
-        offline:{},
-      },
-    }
+
   },
   mutations: {
+
   },
   actions: {
   },
   modules: {
+    vk,
   }
 })
